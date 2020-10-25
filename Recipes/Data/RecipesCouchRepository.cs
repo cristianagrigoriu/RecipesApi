573d68fe8400674a2f62888840e0d8b393878cba
Cristiana Grigoriu <cristiana.grigoriu@centric.eu>
2020-10-25 19:14:03 +0200

    Add class for couch repository

---
+++ Recipes/Data/RecipesCouchRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace Recipes.Data
+{
+    using System.Collections.Generic;
+
+    public class RecipesCouchRepository : IRecipesRepository
+    {
+        public IEnumerable<Recipe> GetAllRecipes()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddRecipe(Recipe newRecipe)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Recipe GetRecipeById(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteRecipe(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateRecipe(Recipe updatedRecipe)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> GetInstructionsOfRecipe(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
