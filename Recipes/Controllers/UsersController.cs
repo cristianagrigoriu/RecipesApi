c12de5ed74d0c635167e52facdc4a0333086c476
Cristiana Grigoriu <cristiana.grigoriu@centric.eu>
2020-12-02 14:08:36 +0200

    Generate token for user

--- Recipes/Controllers/UsersController.cs
+++ Recipes/Controllers/UsersController.cs
@@ -3,7 +3,16 @@
 namespace Recipes.Controllers
 {
     using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
     using Constants;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.DataProtection;
+    using Microsoft.IdentityModel.Tokens;
     using Models;
 
     [Route(MainRoutes.UsersRoute)]
@@ -13,8 +22,26 @@ namespace Recipes.Controllers
         [HttpPost("authenticate")]
         public ActionResult<string> GetTokenForUser(UserModel user)
         {
-            var token = Guid.NewGuid();
+            var token = this.GenerateToken(user.Username);
+
             return Ok(token);
+        }
+        
+        private string GenerateToken(string username)
+        {
+            var secret = "ERMN05OPLoDvbTTa/QkqLNMI7cPLguaRyHzyg7n5qNBVjQmtBhz4SzYh4NBVCXi3KJHlSXKP+oi2+bXr6CUYTR==";
+            var key = Convert.FromBase64String(secret);
+            var securityKey = new SymmetricSecurityKey(key);
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, username)}),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.CreateJwtSecurityToken(descriptor);
+            return jwtSecurityTokenHandler.WriteToken(token);
         }
 
         [HttpPost]
