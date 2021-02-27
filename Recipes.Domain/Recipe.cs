b483181d05c2124fc156e658cf2543d69f2149d8
Cristiana Grigoriu <Cristiana.Grigoriu@centric.eu>
2021-02-27 11:53:37 +0200

    first commit

--- Recipes.Domain/Recipe.cs
+++ Recipes.Domain/Recipe.cs
@@ -8,10 +8,8 @@ namespace Recipes.Domain
     {
         private List<RecipeIngredient> ingredients = new List<RecipeIngredient>();
 
-        //[JsonProperty("_id")]
         public string Id { get; set; }
 
-        //[JsonProperty("_rev")]
         public string Rev { get; set; }
 
         public string Name { get; set; }
--- Recipes/Data/AutoMapping.cs
+++ Recipes/Data/AutoMapping.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using AutoMapper.Internal;
 using Recipes.Models;
 
 namespace Recipes.Data
@@ -10,11 +13,14 @@ namespace Recipes.Data
         public AutoMapping()
         {
             CreateMap<Recipe, RecipeModel>();
-            CreateMap<RecipeModel, Recipe>().
-                ForMember(x => x.Id, opt => opt.Ignore()); //ToDo research
+            CreateMap<RecipeModel, Recipe>().ForMember(x => x.Id, opt => opt.Ignore());
+                //.ForMember(x => x.IngredientList.ForAll(m => m.)); //ToDo research
 
             CreateMap<RecipeIngredient, RecipeIngredientModel>();
             CreateMap<RecipeIngredientModel, RecipeIngredient>();
+            
+            CreateMap<IngredientMeasureModel, IngredientMeasure>();
+            CreateMap<IngredientMeasure, IngredientMeasureModel>();
 
             CreateMap<User, UserModel>();
             CreateMap<UserModel, User>();
