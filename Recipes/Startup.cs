3e65afbdfd86b2354aa1c6fef6d765073512b272
Cristiana Grigoriu <Cristiana.Grigoriu@centric.eu>
2021-04-20 22:33:08 +0300

    Copy folder with views from webb react application, configure dependencies

--- Recipes/Startup.cs
+++ Recipes/Startup.cs
@@ -1,9 +1,13 @@
+using System.IO;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Recipes.Recipes.Data;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 
 namespace Recipes
 {
@@ -27,6 +31,14 @@ namespace Recipes
                 .AddAutoMapper(typeof(Startup))
                 .AddTransient<IRecipesRepository, RecipesRepository>()
                 .AddHttpContextAccessor();
+
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +53,16 @@ namespace Recipes
                 app.ConfigureExceptionHandler();
             }
 
+            //app.UseStaticFiles(new StaticFileOptions
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //        Path.Combine(env.ContentRootPath, "Recipes/StaticFiles")),
+            //    RequestPath = "/recipe"
+            //});
+
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
@@ -49,9 +70,26 @@ namespace Recipes
 
             app.UseAuthorization();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
 
             app.UseSwagger();
